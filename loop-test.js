c05f0217411410fda2ecdfeadc5e1b82ac19edf0
leadbrightloop <95039806+leadbrightloop@users.noreply.github.com>
2022-01-18 13:49:47 -0500

    Delete loop-test.js

--- loop-test.js
+++
@@ -1,7 +0,0 @@
-  window._mfq = window._mfq || [];
-  (function() {
-    var mf = document.createElement("script");
-    mf.type = "text/javascript"; mf.defer = true;
-    mf.src = "//cdn.mouseflow.com/projects/46e29951-e351-4dde-91e6-fae6bb389ea2.js";
-    document.getElementsByTagName("head")[0].appendChild(mf);
-  })();
