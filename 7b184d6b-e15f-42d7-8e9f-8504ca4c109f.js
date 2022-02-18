a87d8fe0972a15a6407b3fddfaff4fc9f60aebb9
leadbrightloop <95039806+leadbrightloop@users.noreply.github.com>
2022-02-18 09:45:02 -0500

    Update 7b184d6b-e15f-42d7-8e9f-8504ca4c109f.js

--- 7b184d6b-e15f-42d7-8e9f-8504ca4c109f.js
+++ 7b184d6b-e15f-42d7-8e9f-8504ca4c109f.js
@@ -1,1 +1,7 @@
-
+window._mfq = window._mfq || [];
+  (function() {
+    var mf = document.createElement("script");
+    mf.type = "text/javascript"; mf.defer = true;
+    mf.src = "//cdn.mouseflow.com/projects/762a2bca-252a-44f9-bf6b-03f1df9591f3.js";
+    document.getElementsByTagName("head")[0].appendChild(mf);
+  })();
