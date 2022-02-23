03ebd3a55dbfb4dcce067c7486e9c029b8653fad
leadbrightloop <95039806+leadbrightloop@users.noreply.github.com>
2022-02-23 13:22:46 -0500

    Update 7b184d6b-e15f-42d7-8e9f-8504ca4c109f.js

--- 7b184d6b-e15f-42d7-8e9f-8504ca4c109f.js
+++ 7b184d6b-e15f-42d7-8e9f-8504ca4c109f.js
@@ -5,3 +5,26 @@ window._mfq = window._mfq || [];
     mf.src = "//cdn.mouseflow.com/projects/762a2bca-252a-44f9-bf6b-03f1df9591f3.js";
     document.getElementsByTagName("head")[0].appendChild(mf);
   })();
+
+<!-- Hotjar Tracking Code for http://www.leadbright.com -->
+<script>
+(function(h,o,t,j,a,r){
+h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+h._hjSettings={hjid:610678,hjsv:6};
+a=o.getElementsByTagName('head')[0];
+r=o.createElement('script');r.async=1;
+r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+
+
+<!-- Clarity code -->
+<script>
+(function(c,l,a,r,i,t,y){
+c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "44v9pn2rn1");
+</script>
+<!-- End Clarity code -->
