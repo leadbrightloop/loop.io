2f86143f1f3405bba20be70236ebaf8a1ecebf26
leadbrightloop <95039806+leadbrightloop@users.noreply.github.com>
2022-04-07 18:11:14 -0400

    phoex

---
+++ 6aaf28a4-25b1-47e0-be35-4ba24f243803.js
@@ -0,0 +1,8 @@
+// mouseflow code
+window._mfq = window._mfq || [];
+  (function() {
+    var mf = document.createElement("script");
+    mf.type = "text/javascript"; mf.defer = true;
+    mf.src = "//cdn.mouseflow.com/projects/3f3f9fb3-1b91-4a85-8ac4-8c1830caf68a.js";
+    document.getElementsByTagName("head")[0].appendChild(mf);
+  })();
