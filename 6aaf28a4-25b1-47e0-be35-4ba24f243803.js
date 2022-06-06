a85724490b8dd8335386ba119a9b9c8effca758a
leadbrightloop <95039806+leadbrightloop@users.noreply.github.com>
2022-06-06 16:47:38 -0400

    Update 6aaf28a4-25b1-47e0-be35-4ba24f243803.js
    added matamo tag manager

--- 6aaf28a4-25b1-47e0-be35-4ba24f243803.js
+++ 6aaf28a4-25b1-47e0-be35-4ba24f243803.js
@@ -8,15 +8,10 @@ window._mfq = window._mfq || [];
   })();
 
 // matoma code
-  var _paq = window._paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="https://leadbright1.matomo.cloud/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '3']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src='//cdn.matomo.cloud/leadbright1.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
-  })();
+var _mtm = window._mtm = window._mtm || [];
+_mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+g.async=true; g.src='https://cdn.matomo.cloud/leadbright1.matomo.cloud/container_g3kUuxdb.js'; s.parentNode.insertBefore(g,s);
+
+
 
