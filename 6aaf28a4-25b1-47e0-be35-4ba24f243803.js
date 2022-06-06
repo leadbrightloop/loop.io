99e8f07a451c7b1ff842b1e07abc7a18975e554b
leadbrightloop <95039806+leadbrightloop@users.noreply.github.com>
2022-06-06 16:44:11 -0400

    Update 6aaf28a4-25b1-47e0-be35-4ba24f243803.js
    Added matoma code

--- 6aaf28a4-25b1-47e0-be35-4ba24f243803.js
+++ 6aaf28a4-25b1-47e0-be35-4ba24f243803.js
@@ -6,3 +6,17 @@ window._mfq = window._mfq || [];
     mf.src = "//cdn.mouseflow.com/projects/3f3f9fb3-1b91-4a85-8ac4-8c1830caf68a.js";
     document.getElementsByTagName("head")[0].appendChild(mf);
   })();
+
+// matoma code
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://leadbright1.matomo.cloud/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '3']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src='//cdn.matomo.cloud/leadbright1.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+
