b8690ce9aafc016e0f3aa860e26fbb2c0ec14553
leadbrightloop <95039806+leadbrightloop@users.noreply.github.com>
2023-01-27 11:54:31 -0800

    Update 412bf156-383d-4fa8-8371-ec69236e818d.js

--- 412bf156-383d-4fa8-8371-ec69236e818d.js
+++ 412bf156-383d-4fa8-8371-ec69236e818d.js
@@ -6,3 +6,15 @@ window._mfq = window._mfq || [];
     mf.src = "//cdn.mouseflow.com/projects/07e2c9bb-d296-41af-81ea-0ef3e42f31b2.js";
     document.getElementsByTagName("head")[0].appendChild(mf);
   })();
+
+// ========== Matomo ==========
+var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://leadbright1.matomo.cloud/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '4']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src='//cdn.matomo.cloud/leadbright1.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
