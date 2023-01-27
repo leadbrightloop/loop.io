ea239ef00eddbc4981a817055945acab7ec1dafc
leadbrightloop <95039806+leadbrightloop@users.noreply.github.com>
2023-01-27 11:42:00 -0800

    Create 412bf156-383d-4fa8-8371-ec69236e818d.js

---
+++ 412bf156-383d-4fa8-8371-ec69236e818d.js
@@ -0,0 +1,8 @@
+// ========== Mouseflow ==========
+window._mfq = window._mfq || [];
+  (function() {
+    var mf = document.createElement("script");
+    mf.type = "text/javascript"; mf.defer = true;
+    mf.src = "//cdn.mouseflow.com/projects/07e2c9bb-d296-41af-81ea-0ef3e42f31b2.js";
+    document.getElementsByTagName("head")[0].appendChild(mf);
+  })();
