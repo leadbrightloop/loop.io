72d648a7fd1a93b60e36ac4dbf06887071be3bf4
leadbrightloop <95039806+leadbrightloop@users.noreply.github.com>
2023-05-03 18:57:06 -0700

    Create 8d8249c0-868e-40c4-95ac-804b01b00cef.js

---
+++ 8d8249c0-868e-40c4-95ac-804b01b00cef.js
@@ -0,0 +1,17 @@
+<!-- Leadbright -->
+<script>
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://leadbright1.matomo.cloud/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '7']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true;
+    g.src='//cdn.matomo.cloud/leadbright1.matomo.cloud/matomo.js';
+    s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Leadbright Code -->
