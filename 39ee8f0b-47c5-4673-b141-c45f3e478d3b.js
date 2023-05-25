649c4d432984e18f0465ddba384e432fa2a10a20
leadbrightloop <95039806+leadbrightloop@users.noreply.github.com>
2023-05-25 10:35:05 -0700

    Create 39ee8f0b-47c5-4673-b141-c45f3e478d3b.js

---
+++ 39ee8f0b-47c5-4673-b141-c45f3e478d3b.js
@@ -0,0 +1,16 @@
+var _paq = window._paq = window._paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(["setCookieDomain", "*.icaninfotech.com"]);
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+  var u="https://leadbright1.matomo.cloud/";
+  _paq.push(['setTrackerUrl', u+'matomo.php']);
+  _paq.push(['setSiteId', '9']);
+  var d=document,
+      g=d.createElement('script'),
+      s=d.getElementsByTagName('script')[0];
+  g.async=true;
+  g.src='//cdn.matomo.cloud/leadbright1.matomo.cloud/matomo.js';
+  s.parentNode.insertBefore(g,s);
+})();
