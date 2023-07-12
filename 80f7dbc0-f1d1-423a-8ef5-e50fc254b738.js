765574ac97e47a120e68f91720ce99e5e44f96df
leadbrightloop <95039806+leadbrightloop@users.noreply.github.com>
2023-07-11 20:34:39 -0700

    Add three trackers

---
+++ 80f7dbc0-f1d1-423a-8ef5-e50fc254b738.js
@@ -0,0 +1,18 @@
+var _paq = window._paq = window._paq || [];
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+  var u="https://leadbright1.matomo.cloud/";
+  _paq.push(['setTrackerUrl', u+'matomo.php']);
+  _paq.push(['setSiteId', '12']);
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.async=true; g.src='//cdn.matomo.cloud/leadbright1.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+})();
+// ==========
+window._mfq = window._mfq || [];
+(function() {
+   var mf = document.createElement("script");
+   mf.type = "text/javascript"; mf.defer = true;
+   mf.src = "//cdn.mouseflow.com/projects/6de7db3e-32d9-4c8f-abab-a673cbb89afd.js";
+   document.getElementsByTagName("head")[0].appendChild(mf);
+})();
